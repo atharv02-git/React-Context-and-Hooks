5af012f579a93334c35037b0efdfe81158e91785
Atharv <atharvbhandare2001@gmail.com>
2022-01-06 15:38:27 +0530

    Accessing Context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends React.Component {
-  render() { 
+  static contextType = ThemeContext; //Accessing context
+  render() {
+    console.log(this.context)
+    const { isLightTheme, light, dark } = this.context; //destructuring properties of the context
+    const theme = isLightTheme ? light : dark;
     return (
-      <div className="book-list">
+      <div className="book-list" style={{ background: theme.bg, color: theme.syntaxColor}}>
         <ul>
-          <li>Game of Thrones</li>
-          <li>Harry Potter</li>
-          <li>Narnia</li>
+          <li style={{ background: theme.ui }}>Game of Thrones</li>
+          <li style={{ background: theme.ui }}>Harry Potter</li>
+          <li style={{ background: theme.ui }}>Narnia</li>
         </ul>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends React.Component {
+  static contextType = ThemeContext; //Accessing context
   render() {
+    console.log(this.context)
+    const { isLightTheme, light, dark } = this.context; //destructuring properties of the context
+    const theme = isLightTheme ? light : dark;
     return (
-      <nav>
+      <nav style={{ background: theme.ui, color: theme.syntaxColor }}>
         <h1>Context App</h1>
         <ul>
           <li>Home</li>
