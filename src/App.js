10e2f4a7ffb739ab372c3ce5a86a8b7291c285c9
Atharv <atharvbhandare2001@gmail.com>
2022-01-06 18:12:00 +0530

    creating multiple context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import BookList from './components/BookList';
-import Navbar from './components/Navbar';
-import ThemeToggle from './components/ThemeToggle';
-import ThemeContextProvider from './contexts/ThemeContext';
+import React from "react";
+import BookList from "./components/BookList";
+import Navbar from "./components/Navbar";
+import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 // context
 
@@ -11,9 +12,11 @@ function App() {
     <div className="App">
       <ThemeContextProvider>
         {/* when we surround like <ThemeContextProvider> using component <Navbar /> and <BookList /> are attached to the props of the component that we wrap, so we can access the children component inside the parent component(i.e: ThemeContextProvider)*/}
-        <Navbar /> 
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,23 @@
+import React, { createContext } from "react";
+
+export const AuthContext = createContext();
+
+class AuthContextProvider extends React.Component {
+  state = {
+    isAuhenticated: false,
+  };
+
+  toggleAuth = () => {
+    this.setState({ isAuhenticated: !this.state.isAuhenticated });
+  };
+
+  render() {
+    return (
+      <AuthContext.Provider value={{ ...this.state, toggleAuth: this.toggleAuth }}>
+        {this.props.children}
+      </AuthContext.Provider>
+    );
+  }
+}
+
+export default AuthContextProvider;
