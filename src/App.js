9591aad74509143253aa79045ef243072caeec4f
Atharv <atharvbhandare2001@gmail.com>
2022-01-07 17:48:08 +0530

    how to create context in functional components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeToggle from "./components/ThemeToggle";
 import AuthContextProvider from "./contexts/AuthContext";
+import BookContextProvider from "./contexts/BookContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 
 // context
@@ -15,7 +16,9 @@ function App() {
         {/* when we surround like <ThemeContextProvider> using component <Navbar /> and <BookList /> are attached to the props of the component that we wrap, so we can access the children component inside the parent component(i.e: ThemeContextProvider)*/}
         <AuthContextProvider>
           <Navbar />
-          <BookList />
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
           <ThemeToggle />
           <AuthToggle />
         </AuthContextProvider>
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
 export default function BookList() {
   const {isLightTheme, light, dark} = useContext(ThemeContext)
+  const {books}  = useContext(BookContext);
   const theme = isLightTheme ? light : dark;
   return (
     <div className="book-list" style={{ background: theme.bg, color: theme.syntaxColor}}>
         <ul>
-          <li style={{ background: theme.ui }}>Game of Thrones</li>
-          <li style={{ background: theme.ui }}>Harry Potter</li>
-          <li style={{ background: theme.ui }}>Narnia</li>
+          {books.map((book) => (
+            <li key={book.id} style={{ background: theme.ui }}>{book.title}</li>
+          ))}
         </ul>
       </div>
   )
